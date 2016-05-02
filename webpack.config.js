4344423ce268e9c604607527599fabf8e92305de
Hendrik Purmann <h.purmann@googlemail.com>
2016-05-02 02:17:08 +0200

    Add .babelrc, configure build targets

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-    context: __dirname + "/lib",
-        entry: "./index.js",
-        output: {
-            path: __dirname + "/bundle",
-            filename: "yourchoice.js",
-            libraryTarget: "umd",
-            library: "Yourchoice"
-        },
-        externals: {
-            "lodash": "_"
-        }
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'Yourchoice',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    'lodash': '_'
+  }
 }
