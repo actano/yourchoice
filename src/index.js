301e4fbae427b9b495795ef71f9f3072fb0440a3
Hendrik Purmann <h.purmann@googlemail.com>
2016-07-18 16:11:22 +0200

    Export functional public interface

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import { Selection } from './selection'
+import {
+  init,
+  setItems,
+  setSelection,
+  replace,
+  toggle,
+  remove,
+  removeAll,
+  rangeTo,
+  getSelection,
+  getChangedSelection,
+  getChangedDeselection,
+} from './operations'
+
 export default Selection
+export {
+  init,
+  setItems,
+  setSelection,
+  replace,
+  toggle,
+  remove,
+  removeAll,
+  rangeTo,
+  getSelection,
+  getChangedSelection,
+  getChangedDeselection,
+}
