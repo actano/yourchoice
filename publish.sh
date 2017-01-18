edd11abc7c3cd080d4457127d11fcb715f5f18c3
Hendrik Purmann <h.purmann@googlemail.com>
2017-01-18 18:46:09 +0100

    Add publish script for gh-pages

---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+yarn run build
+git checkout --orphan temp-gh-pages
+rm .gitignore
+echo "node_modules" > .gitignore
+git reset
+git add index.html
+git add dist
+git add static
+git commit -m "Update gh-pages"
+
+git clean -df
+
+git push -f origin temp-gh-pages:gh-pages
+git checkout website
+git branch -D temp-gh-pages
