41925ac5396fbb594ddf7db4b9050eb6e7a0020f
Hendrik Purmann <h.purmann@googlemail.com>
2017-01-25 00:30:52 +0100

    Add webpack DLL plugin
    This makes re-builds faster because it caches the vendor bundle

---
+++ vendor.webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    vendor: [
+      'react',
+      'react-bootstrap',
+      'react-dom',
+      'react-redux',
+    ],
+  },
+  output: {
+    filename: 'vendor.bundle.js',
+    path: 'dist/',
+    library: 'vendor_lib',
+  },
+  performance: {
+    hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      name: 'vendor_lib',
+      path: 'dist/vendor-manifest.json',
+    }),
+  ],
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   entry: './src/app.jsx',
   output: {
     path: path.join(__dirname, 'dist/'),
     publicPath: 'dist',
-    filename: 'bundle.js',
+    filename: 'app.bundle.js',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -22,5 +23,11 @@ module.exports = {
   performance: {
     hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
   },
+  plugins: [
+    new webpack.DllReferencePlugin({
+      context: '.',
+      manifest: require('./dist/vendor-manifest.json'),
+    }),
+  ],
   devtool: 'source-map',
 }
